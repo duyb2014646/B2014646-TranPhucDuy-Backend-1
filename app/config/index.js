cc9458875766a963f3a9587f852cb015eb6853e0
duy <duyb2014646@student.ctu.edu.vn>
2024-03-07 00:10:01 +0700

    Cai dat cac handler truy xuat CSDL

--- app/config/index.js
+++ app/config/index.js
@@ -1,4 +1,8 @@
 const config = {
+    db: {
+        uri: process.env.MONGODB_URI || "mongodb+srv://test:test@cluster0.olrqmwy.mongodb.net/",
+    },
+
     app: {
         port: process.env.PORT || 3000,
     }
